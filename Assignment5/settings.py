26e4d5fe15e303aca1f37b0ba22e6e533d668f01
Md Rakib Bhuiyan <mrb@mrb.mrb>
2024-06-22 23:43:28 +0600

    trying to deploy

--- Assignment5/settings.py
+++ Assignment5/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -159,5 +160,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rakib2046.md@gmail.com'
-# EMAIL_HOST_PASSWORD = 
+EMAIL_HOST_USER = os.getenv('EMAIL')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAILPASS')
