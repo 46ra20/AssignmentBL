d63364cb4997171711042c45e800ccb6ef1e7e22
Md Rakib Bhuiyan <mrb@mrb.mrb>
2024-06-22 12:51:26 +0600

    i cann't show image in my website

--- Assignment5/settings.py
+++ Assignment5/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for Assignment5 project.
 
@@ -11,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,9 +126,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = '/Media/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "Media"),
+)
+
+MEDIA_ROOT = (
+BASE_DIR
+)
+
+
+MEDIA_URL = '/media/'
