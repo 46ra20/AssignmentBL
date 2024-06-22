ffac9cbe5756aac50f5f9144bb276c8242e72408
Md Rakib Bhuiyan <mrb@mrb.mrb>
2024-06-22 21:55:31 +0600

    allowed hosts

--- Assignment5/settings.py
+++ Assignment5/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-@1m0j%cx(w6kjfyp=%^+b-=iw54^@xx7*p$j4^91m!o@ar*uii
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'assignmentbl.onrender.com',
+]
 
 
 # Application definition
